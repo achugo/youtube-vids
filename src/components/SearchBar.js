b9bc72d982e13c8c614d0430533b6d9797c31db0
achugo <achugo2017@gmail>
2019-05-30 02:15:00 +0100

    prevent default refresh on form when enter key is pressed

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -4,13 +4,20 @@ import React from 'react';
 class SearchBar extends React.Component {
     state = {myInput : '' }
 
-    onInputChange = () => {
+    onInputChange = (event) => {
+        //update input state with value of the text in input field
+        this.setState({myInput: event.target.value})
+    }
 
+    onFormSubmit = (event) => {
+        event.preventDefault()
+
+        //TODO : Call the call back from the parent component
     }
     render(){
         return (
             <div className="search-bar ui segment">
-                <form className="ui form">
+                <form onSubmit={this.onFormSubmit} className="ui form">
                     <div className="field">
                         <label>Video Search</label>
                         <input type="text" onChange={this.onInputChange} value={this.state.term} />
