78fdb1efbf8d4ae0fd98ace73fbfc664af1c096c
achugo <achugo2017@gmail>
2019-09-22 12:49:20 +0100

    Updates

---
+++ precache-manifest.bf2aa89a61267da64a6e00fa93111883.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8c14b3bb92ed851404abceec58418f83",
+    "url": "/youtube-vids/index.html"
+  },
+  {
+    "revision": "9281fbc951acbeafa1d4",
+    "url": "/youtube-vids/static/css/main.8d43f077.chunk.css"
+  },
+  {
+    "revision": "7e6ac1444ef9242ebc00",
+    "url": "/youtube-vids/static/js/2.824d5390.chunk.js"
+  },
+  {
+    "revision": "9281fbc951acbeafa1d4",
+    "url": "/youtube-vids/static/js/main.ed4d46f7.chunk.js"
+  },
+  {
+    "revision": "faa95957db2952b338b9",
+    "url": "/youtube-vids/static/js/runtime~main.416d38c1.js"
+  }
+]);
